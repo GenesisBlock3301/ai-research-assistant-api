28d71082103d3cc8cd24a00a6bf4db87b3cab62c
genesisblock3301 <nur15-1463@diu.edu.bd>
2025-11-05 15:14:54 +0600

    add cors middleware

--- app/main.py
+++ app/main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1 import user_router, chat_router, document_router
 from app.middlewares import AuthMiddleware
 
+
+origins = [
+    "http://localhost:3000",  # your frontend URL
+    "http://127.0.0.1:3000",  # optional, same as above
+]
 app = FastAPI(title="Personal Research Agent")
 
-app.add_middleware(AuthMiddleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(
+    AuthMiddleware
+)
 app.include_router(user_router, prefix="/api/v1/users", tags=["users"])
 app.include_router(chat_router, prefix="/api/v1/chats", tags=["chats"])
 app.include_router(document_router, prefix="/api/v1/documents", tags=["documents"])
